cb54b14c357a6204584e466ff2274160aabf90df
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-04-14 08:31:33 -0700

    Add test code for update, change status and clear completed functions

--- src/modules/Functionalities.test.js
+++ src/modules/Functionalities.test.js
@@ -16,4 +16,23 @@ describe('test add and remove', () => {
 
     expect(result).toBe(1);
   });
+  test('edit task', () => {
+    const text = 'Go home';
+    const result = updateList(1, text);
+
+    expect(result).toMatch(/Go home/);
+  });
+
+  test('clear completed', () => {
+    const result = clearCompleted();
+
+    expect(result).toBe(1);
+  });
+
+  test('status updated', () => {
+    const state = true;
+    const result = updateCompleteTodo(state, 0);
+
+    expect(result).toBeTruthy();
+  });
 });
